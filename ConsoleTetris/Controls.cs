b1a1ae863879635e74278cda9381601c2f494662
qqgameover <kasper35@live.no>
2021-10-31 16:58:43 +0100

    x

--- ConsoleTetris/Controls.cs
+++ ConsoleTetris/Controls.cs
@@ -13,21 +13,21 @@ namespace ConsoleTetris
         {
             if (Console.KeyAvailable == false) return new Vector2(0f, 1f);
             var cki = Console.ReadKey(true);
-            if (cki.Key == ConsoleKey.W)
+            if (cki.Key == ConsoleKey.UpArrow)
             {
                 p.UndrawBlock(p.Position, MapMang.Manager.BoardArray);
                 p.RotateBlockMatrix();
                 return new Vector2(0f, 1f);
             }
-            if (cki.Key == ConsoleKey.R)
+            if (cki.Key == ConsoleKey.DownArrow)
             {
                 return new Vector2(0f, 1f);
             }
-            if (cki.Key == ConsoleKey.A)
+            if (cki.Key == ConsoleKey.LeftArrow)
             {
                 return new Vector2(-1f, 1f);
             }
-            if (cki.Key == ConsoleKey.S)
+            if (cki.Key == ConsoleKey.RightArrow)
             {
                 return new Vector2(1f, 1f);
             }
